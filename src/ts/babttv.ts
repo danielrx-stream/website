39d99a29b1c81f0df59bfed965f8c5261a3036e8
DanielRX <16984675+DanielRX@users.noreply.github.com>
2021-03-26 19:56:25 +0000

    Merge saveSvgAsPng into src

--- src/ts/babttv.ts
+++ src/ts/babttv.ts
@@ -5,6 +5,65 @@ type PickrCol = string | {toHEXA(): string[]} | undefined;
 let pickerColour: PickrCol;
 let pickr: any;
 let lastLayerColour: PickrCol;
+// !------------------------------------------------------------------------------------------
+
+
+const doctype = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd" [<!ENTITY nbsp "&#160;">]>';
+
+const reEncode = (data) => decodeURIComponent(encodeURIComponent(data).replace(/%([0-9A-F]{2})/g, (match, p1) => { const c = String.fromCharCode(`0x${p1}`); return c === '%' ? '%25' : c; }));
+
+const prepareSvg = (el, scale: number) => {
+    let clone = el.cloneNode(true);
+
+    clone.setAttribute('width', 112 * scale);
+    clone.setAttribute('height', 112 * scale);
+
+    const validStyle = (sheet: CSSStyleSheet) => (sheet.href?.endsWith('svg-style.css')) || (sheet.href === null);
+    const validRule = (rule: CSSStyleRule) => 'style' in rule && el.querySelector(rule.selectorText);
+    const sheetToString = (sheet: CSSStyleSheet) => Array.from(sheet.cssRules).filter(validRule).map((rule) => `${rule.selectorText}{${rule.style.cssText}}\n`).join('\n')
+    const css = Array.from(document.styleSheets).filter(validStyle).map(sheetToString).join('\n');
+
+    const style = document.createElement('style');
+    style.setAttribute('type', 'text/css');
+    style.innerHTML = `<![CDATA[\n${css}\n]]>`;
+
+    const defs = document.createElement('defs');
+    defs.appendChild(style);
+    clone.insertBefore(defs, clone.firstChild);
+
+    const outer = document.createElement('div');
+    outer.appendChild(clone);
+    return outer.innerHTML.replace(/NS\d+:href/gi, 'xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href');
+};
+
+const convertToPng = (src: CanvasImageSource, scale: number) => {
+    const pixelRatio = window.devicePixelRatio || 1;
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d')!;
+    canvas.width = scale * 112 * pixelRatio;
+    canvas.height = scale * 112 * pixelRatio;
+    canvas.style.width = `${canvas.width}px`;
+    canvas.style.height = `${canvas.height}px`;
+    context.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
+    context.drawImage(src, 0, 0);
+    const x = canvas.toDataURL('image/png', 0.8); // <---- time sink
+    return x;
+}
+
+
+const svgAsPngUri = (emoteName: string, scale: number): () => Promise<string> => {
+    const el = document.getElementById(showEmote(emoteName)!.attr('id')!);
+    const src = prepareSvg(el, scale);
+    return () => {
+        return new Promise((resolve) => {
+            const image = new Image();
+            image.onload = () => resolve(convertToPng(image, scale));
+            image.src = `data:image/svg+xml;base64,${window.btoa(reEncode(doctype+src))}`;
+        })
+    };
+};
+// !------------------------------------------------------------------------------------------
+
 
 const titleCase = (s: string) => s[0]!.toUpperCase() + s.slice(1);
 
@@ -54,14 +113,16 @@ const showHair = (hairType?: string) => {
     $(`[id^="core-hair-${hairType}"]`).show();
 };
 
-const showEmote = (emoteNameForce: string) => {
+const showEmote = (emoteNameForce: string): any => {
     const emoteName = getEmoteName(emoteNameForce);
     if(emoteName) {
         $('[id^="variant"]').hide();
         $('[id^="variant"]').parents('svg').hide();
         showHair();
         $(`[id^="variant-${emoteName}"]`).show();
-        getSVGOfEmote(emoteName).show();
+        const svg = getSVGOfEmote(emoteName);
+        svg.show();
+        return svg;
     }
 };
 
@@ -97,28 +158,24 @@ const hideOverview = (emoteName: string) => () => {
     $('.overview').css({'z-index': -2});
 };
 
-const addToOverview = async(emoteName: string) => {
-    const emote = $(`[id^="variant-${emoteName}"]`);
-    const id = emote.parents('svg').attr('id')!;
-    const uri = await (window as any).svgAsPngUri(document.getElementById(id), {scale:1.5, excludeUnusedCss: true});
-    $('#emote-grid').append(`<img width="140px" src="${uri}" id="emote-overview-${emoteName}" style="display:none" class="breathing"/>`);
-    $(`#emote-overview-${emoteName}`).on('click', hideOverview(emoteName));
-};
-
 const addAllToOverview = async() => {
+    console.time('AddAll');
     const oldEmote = getEmoteName();
     $('#emote-wrapper').hide();
     $('#emote-grid img').remove();
-    for(const emoteName of getEmoteNames()) {
-        showEmote(emoteName);
-        await addToOverview(emoteName);
+    const emoteCalls = [];
+    const emoteNames = getEmoteNames();
+    for(const emoteName of emoteNames) {
+        $('#emote-grid').append(`<img width="140px" src="" id="emote-overview-${emoteName}" style="display:none" class="breathing"/>`);
+        $(`#emote-overview-${emoteName}`).on('click', hideOverview(emoteName));
+        emoteCalls.push(svgAsPngUri(emoteName, 15));
     }
     showEmote(oldEmote);
     $('#emote-wrapper').show();
+    await Promise.all((emoteCalls.map((f, i) => f().then((uri: string) => $(`#emote-overview-${emoteNames[i]}`).attr('src', uri))))).then(() => { console.timeEnd('AddAll'); });
 };
 
 // !------------------------------------------------------------------------------------------
-
 
 const setEmoteLayers = (emoteNameForce?: string) => {
     const emoteName = getEmoteName(emoteNameForce);
@@ -144,6 +201,7 @@ const getEmoteSizes = () => {
     }
 };
 
+
 const downloadEmotes = async() => {
     const emoteSizes = getEmoteSizes();
     const zip = new (window as any).JSZip();
@@ -152,11 +210,8 @@ const downloadEmotes = async() => {
     $('#emote-wrapper').hide();
     let j = 0;
     for(const emoteName of emoteNames) {
-        showEmote(emoteName);
-        const emote = $(`[id^="variant-${emoteName}"]`);
-        const id = emote.parents('svg').attr('id')!;
         for(const size of emoteSizes) {
-            const uri = await (window as any).svgAsPngUri(document.getElementById(id), {scale: size/112})
+            const uri = await svgAsPngUri(emoteName, size/112)();
             const idx = uri.indexOf('base64,') + 'base64,'.length;
             const content = uri.substring(idx);
             zip.file(`${size}/${emoteName}.png`, content, {base64: true});
@@ -191,6 +246,7 @@ window.onload = () => {
     const init = () => {
         $('#emote-name').text(titleCase(getEmoteNames()[0]));
         setEmoteLayers();
+        setTimeout(() => $('#overview-link').click(), 1000);
     };
 
     setInterval(colourLayerIn, 100);
